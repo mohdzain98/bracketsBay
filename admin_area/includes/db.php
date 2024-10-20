a55ef32974f18232c28f75be49fe35b2801f2a13
Mohd Zain <zainmohd1998@gmail.com>
2024-10-20 12:48:45 +0530

    add env variables

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -1,3 +1,11 @@
 <?php
-$con=(mysqli_connect("localhost","root","Geeky@Zain98","bracketsbay"))
+require __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/../../');
+$dotenv->load();
+
+$uname = $_ENV['UNAME'];
+$DB_NAME=$_ENV['DB_NAME'];
+$DB_PASS=$_ENV['DB_PASS'];
+$con=(mysqli_connect("localhost",$uname,$DB_PASS,$DB_NAME))
 ?>
